79fd1fa5746b0e81b69c8b70a6814f7bbe6d4576
Utku Sarioglu <32932503+utkusarioglu@users.noreply.github.com>
2022-10-31 08:21:38 +0300

    Initial commit

---
+++ scripts/kubectl-watch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source scripts/config.sh || exit 1
+check_env
+check_repo_config
+
+resources=${1:-$DEFAULT_KUBECTL_WATCH_RESOURCES}
+namespace_sorted="po svc ds sts"
+wide="po no sts ing"
+
+echo "watch '\
+  for resource in $resources; do \
+    echo \$resource; \
+    kubectl get \$resource \
+      -A \
+      \$(if echo "$namespace_sorted" | grep -q \$resource; then \
+        echo \"--sort-by=.metadata.namespace\"; \
+      fi) \
+      \$(if echo "$wide" | grep -q \$resource; then \
+        echo \"-o=wide\"; \
+      fi) \
+      ; \
+    echo; \
+  done\
+'" | \
+tr -s " " | \
+sh
